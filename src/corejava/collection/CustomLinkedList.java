4b83cb724ccd3e8e2c290807802f910b577b5d33
sravanthi <sravanthik07@gmail.com>
2021-06-01 22:51:37 +0530

    new learns

--- src/corejava/collection/CustomLinkedList.java
+++ src/corejava/collection/CustomLinkedList.java
@@ -22,36 +22,53 @@ public class CustomLinkedList {
 			n = n.next;
 		}
 	}
-	
+
+	public void add(Object data) {
+
+		Node newNode = new Node(data);
+
+		if (head == null) {
+			head = newNode;
+		}
+
+		else {
+			Node n = head;
+			while (n.next != null) {
+				n = n.next;
+			}
+			n.next = newNode;
+		}
+	}
+
 	public Node reverse(Node node) {
 
-		Node prev = null,next = null;
+		Node prev = null, next = null;
 		Node current = node;
-		
-		while(current != null) {
+
+		while (current != null) {
 			next = current.next;
 			current.next = prev;
 			prev = current;
 			current = next;
 		}
-		
+
 		head = prev;
 		return head;
 	}
 
-	public static void main(String[] args) {
-
-		CustomLinkedList llist = new CustomLinkedList();
-		llist.head = new Node("one");
-		Node second = new Node("two");
-		llist.head.next = second;
-		Node third = new Node("three");
-		second.next = third;
-
-		llist.printList();
-		llist.reverse(llist.head);
-		System.out.println("Reverese linked list");
-		llist.printList();
-	}
+//	public static void main(String[] args) {
+//
+//		CustomLinkedList llist = new CustomLinkedList();
+//		llist.add("one");
+//		llist.add("two");
+//		llist.add(4);
+//		llist.add(null);
+//		llist.add(null);
+//
+//		llist.printList();
+//		llist.reverse(llist.head);
+//		System.out.println("Reverese linked list");
+//		llist.printList();
+//	}
 
 }
---
+++ src/corejava/collection/StackUsingLL.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package corejava.collection;
+
+/**
+ * @author Sravanthikondru
+ *
+ */
+public class StackUsingLL {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
